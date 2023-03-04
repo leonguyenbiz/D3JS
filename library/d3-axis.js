eceb7ded526993de51738dabda28ea85764b8292
Nguyễn Tuấn Trường <35694043+leonguyenbiz@users.noreply.github.com>
2023-03-04 07:41:31 +0700

    Add files via upload

---
+++ library/d3-axis.js
@@ -0,0 +1,10 @@
+/*
+ * Skypack CDN - d3-axis@3.0.0
+ *
+ * 📙 Package Documentation: https://www.skypack.dev/view/d3-axis
+ * 📘 Skypack Documentation: https://www.skypack.dev/docs
+ */
+
+// Browser-Optimized Imports (Don't directly import the URLs below in your application!)
+export * from '/-/d3-axis@v3.0.0-atdNID4TA9Y0wR53lsTq/dist=es2020,mode=imports/optimized/d3-axis.js';
+export {default} from '/-/d3-axis@v3.0.0-atdNID4TA9Y0wR53lsTq/dist=es2020,mode=imports/optimized/d3-axis.js';
